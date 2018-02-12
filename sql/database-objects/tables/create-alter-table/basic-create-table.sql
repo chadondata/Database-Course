0fb4b9116c763c6fea419b97c34785bcfa4dc324
Chad Harper <chad.harper@gmail.com>
2018-02-12 17:40:02 -0500

    Basic Create Table

---
+++ sql/database-objects/tables/create-alter-table/basic-create-table.sql
@@ -0,0 +1,28 @@
+/*
+	Basic CREATE TABLE
+	Author: Chad Harper
+	License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
+	GitHub: https://github.com/chadondata/Database-Course
+
+	ChangeLog
+	2/12/2018 File Created
+
+*/
+CREATE TABLE PostCategory (
+	PostCategoryID int identity NOT NULL
+	, PostCategoryText varchar(50) NOT NULL
+	, PostCategoryDescription varchar(200) NOT NULL
+)
+GO
+
+CREATE TABLE Post (
+	PostID int identity NOT NULL
+	, PostText varchar(240) NOT NULL
+	, PostTimestamp datetime NOT NULL
+	, Post_UserID int NOT NULL
+	, Post_PostCategoryID int 
+)
+GO
+
+SELECT * FROM PostCategory
+SELECT * FROM Post
